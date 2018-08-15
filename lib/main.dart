cb43d5e93caec42ba726098a3c9990408e214d51
Tin Trinh <trinh.tin@hamee.co.jp>
2018-08-15 12:56:46 +0900

    add random list

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(title: Text('Welcome to Flutter')),
         body: Center(
           child: RandomWords(),
         ),
@@ -19,16 +18,69 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final Set<WordPair> _saves = Set<WordPair>();
   @override
-    Widget build(BuildContext context) {
-      final wordPair = WordPair.random();
-      return Text(wordPair.asPascalCase);
-    }
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+          actions: <Widget>[
+            new IconButton(icon: const Icon(Icons.list), onPressed: _pushSave,)
+          ],),
+        body: Center(
+          child: _buildSuggestion(),
+        ),
+    );
+  }
+
+  _pushSave() {
+    print("Click _pushSave()");
+  }
+
+  _buildSuggestion() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildListTile(_suggestions[index]);
+        });
+  }
+
+  _buildListTile(WordPair wordPair) {
+    final alreadySaved = _saves.contains(wordPair);
+    return ListTile(
+      title: Text(
+        wordPair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState((){
+          if (alreadySaved) {
+            _saves.remove(wordPair);
+          } else {
+            _saves.add(wordPair);
+          }
+        });
+      },
+    );
+  }
 }
+
 class RandomWords extends StatefulWidget {
   @override
-    RandomWordsState createState() {
-      // TODO: implement createState
-      return new RandomWordsState();
-    }
+  RandomWordsState createState() {
+    // TODO: implement createState
+    return new RandomWordsState();
+  }
 }
